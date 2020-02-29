5be1e8bbe890de902e15339973bcf57794fe9694
Byron Lopez <bj@localhost.localdomain>
2020-02-29 17:59:29 -0600

    escritura del archivo, escritura de la bitacora y paso de parametros a archivo utils

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 from flask import Flask, request, url_for, render_template
-
+from utils import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,18 @@ def analizar():
     f = request.files['file']
     print(request.files)
     print(f.mimetype)
+    print(type(f))
+    f.save('./'+f.filename)
+    params = request.form
+    #Vamos a inicializar con los datos del form las variables que usa el algortirmo(variables en utils.py)
+        #Tenemos que seleccionar el valor segun el criterio de finalizacion seleccionado
+    valCriterioFinalizacion = params['valcriterio1'] if params['criterio'] == '1'else ''
+    valCriterioFinalizacion = params['valcriterio2'] if params['criterio'] == '2'else valCriterioFinalizacion
+    valCriterioFinalizacion = params['valcriterio3'] if params['criterio'] == '3'else valCriterioFinalizacion 
+    
+    init('alv',params['criterio'],valCriterioFinalizacion,params['criteriop'])
+    escribir_en_bitacora()
+    print("-----------------")
     return "OK"
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,46 @@
+import datetime
+#Aqui tendria que ir la logica del codigo genetico
+_nombreDoc = ''
+_criterioFinalizacion = ''
+_valFinalizacion = 0
+_metodoSeleccion = ''
+_generaciones = 0
+_mejorSolucion = 0
+#Con este metodo inicializamos las variables globales
+def init(nombre,criterio,valcriterio,metodo):
+    global _nombreDoc,_criterioFinalizacion,_valFinalizacion,_metodoSeleccion,_generaciones,_mejorSolucion 
+    _nombreDoc = nombre
+    _criterioFinalizacion = criterio
+    _valFinalizacion = int(valcriterio)
+    _metodoSeleccion = metodo
+    _generaciones = 0
+    _mejorSolucion = 0
+    print('Inicializacion-----')
+    
+
+def escribir_en_bitacora():
+    global _nombreDoc,_criterioFinalizacion,_valFinalizacion,_metodoSeleccion,_generaciones,_mejorSolucion 
+    
+    f = open("bitacora.txt", "a")
+    tiempo = datetime.datetime.now()
+    strfinalizacion = 'Maximo numero de Generaciones-- '+str(_valFinalizacion) if _criterioFinalizacion == '1' else 'default'
+    strfinalizacion = 'El mejor fitness de la poblacion-- '+str(_valFinalizacion) if _criterioFinalizacion == '2' else strfinalizacion
+    strfinalizacion = 'El fitness promedio de la poblacion-- '+str(_valFinalizacion) if _criterioFinalizacion == '3' else strfinalizacion 
+    
+    strseleccion = 'Torneos, escojiendo el de mejor fitness' if _metodoSeleccion == '1' else 'default'
+    strseleccion = 'Los mejores padres(mejor fitness)' if _metodoSeleccion == '2' else strseleccion
+    strseleccion = 'Aleatorios' if _metodoSeleccion == '3' else strseleccion
+    contenido = [
+        
+        'Fecha de ejecución: '+tiempo.date().isoformat()+'\n',
+        'Hora de ejecución: '+tiempo.time().isoformat()+'\n',
+        
+        'Nombre del documento de datos utilizado: ',_nombreDoc+'\n',
+        'Criterio de finalización utilizado: ',strfinalizacion+'\n',
+        'Método de selección: ',strseleccion,'\n',
+        'No. Generaciones: ',str(_generaciones)+'\n',
+        'Mejor solucion: ',str(_mejorSolucion)+'\n',
+        '------------------------------\n',
+        ] 
+    f.writelines(contenido)
+    f.close()    
